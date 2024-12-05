e6d0d29e049c7c728d2254ab7f7466376cec04f9
하 윤서 <hayunseo14@gmail.com>
2024-12-05 13:51:02 +0900

    241205-소프트웨어공학

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -3,5 +3,8 @@ package mygit;
 public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println("This is the 2nd version.");
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");
 	}
 }
